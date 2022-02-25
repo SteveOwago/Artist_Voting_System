c4cae98227ea3a629a70af6e1e5be5626f31540c
SteveOwago <steveowago@gmail.com>
2022-02-25 13:03:33 +0300

    OTP Middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -25,26 +25,26 @@ Route::get('otp', [App\Http\Controllers\OTPController::class, 'index'])->name('o
 Route::post('otp', [App\Http\Controllers\OTPController::class, 'store'])->name('otp.post');
 Route::get('otp/reset', [App\Http\Controllers\OTPController::class, 'resend'])->name('otp.resend');
 
-Route::get('approval-disaproval-logs', 'HomeController@approvalDissaprovalLogs')->name('approval.disaproval.logs');
-Route::get('artists', 'HomeController@artists')->name('artists');
-Route::get('sportstars', 'HomeController@sportstars')->name('sportstars');
-Route::get('judges', 'HomeController@judges')->name('judges');
-Route::get('judges/create', 'HomeController@create_judge')->name('create_judge');
-Route::post('judges/create', 'HomeController@add_judge')->name('add_judge');
+Route::get('approval-disaproval-logs', 'HomeController@approvalDissaprovalLogs')->name('approval.disaproval.logs')->middleware('otp');
+Route::get('artists', 'HomeController@artists')->name('artists')->middleware('otp');
+Route::get('sportstars', 'HomeController@sportstars')->name('sportstars')->middleware('otp');
+Route::get('judges', 'HomeController@judges')->name('judges')->middleware('otp');
+Route::get('judges/create', 'HomeController@create_judge')->name('create_judge')->middleware('otp');
+Route::post('judges/create', 'HomeController@add_judge')->name('add_judge')->middleware('otp');
 
-Route::get('profile/{id}', 'HomeController@profile')->name('profile');
-Route::get('edit_profile/{id}', 'HomeController@edit_profile')->name('edit_profile');
+Route::get('profile/{id}', 'HomeController@profile')->name('profile')->middleware('otp');
+Route::get('edit_profile/{id}', 'HomeController@edit_profile')->name('edit_profile')->middleware('otp');
 Route::put('edit_profile/{id}', 'HomeController@update_profile')->name('update_profile');
 
-Route::post('approve/{id}','HomeController@approve')->name('approve');
-Route::post('disapprove/{id}','HomeController@disapprove')->name('disapprove');
-Route::delete('delete/{id}','HomeController@delete_artist')->name('delete');
+Route::post('approve/{id}','HomeController@approve')->name('approve')->middleware('otp');
+Route::post('disapprove/{id}','HomeController@disapprove')->name('disapprove')->middleware('otp');
+Route::delete('delete/{id}','HomeController@delete_artist')->name('delete')->middleware('otp');
 
-Route::get('vote', 'IndexController@index')->name('vote');
-Route::get('vote/{id}', 'IndexController@vote')->name('voteArtist');
-Route::post('submit_vote', 'IndexController@voteArtist')->name('submit_vote');
+Route::get('vote', 'IndexController@index')->name('vote')->middleware('otp');
+Route::get('vote/{id}', 'IndexController@vote')->name('voteArtist')->middleware('otp');
+Route::post('submit_vote', 'IndexController@voteArtist')->name('submit_vote')->middleware('otp');
 
 
-Route::get('levels','PhasesController@index')->name('levels');
-Route::get('levels/artists/{id}','PhasesController@artistsLevel')->name('levels.artists');
-Route::get('levels/sportstars/{id}','PhasesController@sportstarsLevel')->name('levels.sportstars');
+Route::get('levels','PhasesController@index')->name('levels')->middleware('otp');
+Route::get('levels/artists/{id}','PhasesController@artistsLevel')->name('levels.artists')->middleware('otp');
+Route::get('levels/sportstars/{id}','PhasesController@sportstarsLevel')->name('levels.sportstars')->middleware('otp');
